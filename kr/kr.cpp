734e86b172ed63cedecf4ed1a8d23e4ba6153c5f
Ильхам Муратшин <PETA4OK96@mail.ru>
2021-11-28 22:20:37 +0500

    свет по линиям

--- kr/kr.cpp
+++ kr/kr.cpp
@@ -16,65 +16,45 @@ class Shapee: public CircleShape {
 private:
     int r, R, G, B;
     float x, y;
-    CircleShape c2;
+    CircleShape c1;
 
 
 public:
     
 
-    Shapee(int R, int G, int B) {
+    Shapee(int R, int G, int B, float x, float y) {
         R = R; G = G; B = B;
         this->setFillColor(Color(R, G, B));
         r = abs(rand() % 51 - 100);
         this->setRadius(r);
-        this->setPosition(1920-(rand() % 960), rand() % 1080);
-        //this->setPosition(1693,41);
+        this->setPosition(x,y);
+        this->setOrigin(r,r);
     }
     
-
-    //пересечение
-    int do_intersect(CircleShape c1){
-        this->c2 = c2;
-        float s = sqrt((pow((c1.getPosition().x) - c2.getPosition().x, 2)) + 
-            (pow((c1.getPosition().y) - c2.getPosition().y, 2)));
-        if ((c1.getRadius() + c2.getRadius()) >= s) {
-            c2.setPosition(1920 - (rand() % 960), rand() % 1080);
-            return true;
-        }
-        return false;
-    }
 };
 
+float check_in_circle(float x1, float x2, float y1, float y2)
+{
+    return(sqrt((pow(x1 - x2, 2)) + (pow(y1 - y2, 2))));
+}
 
+float length(float x1, float x2)
+{
+    return (sqrt(pow(x1, 2) + pow(x2, 2)));
+}
 
 
 int main()
 {
-    int n, fps = 30;
-    int phi = 30;
-    cin >> n;
+    int phi = 30 , fps = 30;
     srand(time(NULL));
    
    
-    //RenderWindow window(VideoMode(1920, 1080), "xD")
     RenderWindow window(VideoMode(), "курсовая", Style::Fullscreen);
     window.setFramerateLimit(fps);
     
 
-    vector <Shapee> circles;
-
-   //вектор объектов
-    Shapee concrete_circle(250, 250, 0);
-    circles.push_back(concrete_circle);
-    for (int i = 0; i < n - 1; i++) {
-        circles.push_back(Shapee(250, 250, 0));
-    }
-    
-    
-    
-    
-
-    
+    vector <Shapee> circles;    
 
     while (window.isOpen())
     {
@@ -87,11 +67,33 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
+        Vector2i position = Mouse::getPosition();
+
+        //добавление объектов при помощи мыши
+        
+        
+        
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            circles.push_back(Shapee(250, 250, 0, position.x, position.y));
+        }
+        
+        if (Mouse::isButtonPressed(Mouse::Right))
+        {
+            for (int i = 0; i < circles.size(); i++)
+            {
+                
+                float length = check_in_circle(position.x, circles[i].getPosition().x, position.y, circles[i].getPosition().y);
+                if (length <= circles[i].getRadius())
+                {
+                    circles.erase(circles.begin() + i);
+                }
+            }
+        }
         
         
         //лучик
         //поворот
-        
         float X1, Y1, X2, Y2;
         if (Keyboard::isKeyPressed(Keyboard::Up)) {
             
@@ -101,49 +103,87 @@ int main()
 
             phi = phi - 1;
         }
-        VertexArray line(Quads, 4);
-        Vector2i position = Mouse::getPosition();
+
+        /*VertexArray line(Triangles, 3);
         X1 = cos(phi * M_PI/180) * position.x - sin(phi * M_PI / 180) * position.y;
         Y1 = sin(phi * M_PI / 180) * position.x + cos(phi * M_PI / 180) * position.y;
         X2 = cos(phi * M_PI / 180) * position.x + sin(phi * M_PI / 180) * position.y;
         Y2 = -sin(phi * M_PI / 180) * position.x + cos(phi * M_PI / 180) * position.y;
-        line[0].position = Vector2f(0, 538.5);
-        line[1].position = Vector2f(0, 542.5);
-        line[2].position = Vector2f(X1, Y1);
-        line[3].position = Vector2f(X2, Y2);
+        line[0].position = Vector2f(0, 0);
+        line[1].position = Vector2f(X1, Y1);
+        line[2].position = Vector2f(X2, Y2);
 
         line[0].color = Color::White;
-        line[1].color = Color::White;
-        line[2].color = Color(255, 255, 255, 0);
-        line[3].color = Color(255, 255, 255, 0);
+        line[1].color = Color(255, 255, 255, 0);
+        line[2].color = Color(255, 255, 255, 0);*/
 
-        //цикл
-        /*while (true) {
-
-            Vector2i position = Mouse::getPosition();
-            line[2].position = Vector2f(X1, Y1);
-            line[3].position = Vector2f(X2, Y2);
-            line[2].color = Color(255, 255, 255, 0);
-            line[3].color = Color(255, 255, 255, 0);
-
-            if (Mouse::isButtonPressed(Mouse::Left))
-            {
-                break;
-            }
-            
-        }*/
-
+        int ksi = phi;
         
-
+        
 
 
         //отрисовка объектов
         window.clear(Color::Black);
-        for (auto& s:circles){
-            window.draw(s);
+
+
+        
+
+        //лайны
+        VertexArray lines_1(LinesStrip, 2);
+        
+        for (int i = 0; i < 2 * phi; ++i)
+        {
+            ksi--;
+            X1 = cos(ksi * M_PI / 180) * position.x + sin(ksi * M_PI / 180) * position.y;
+            Y1 = -sin(ksi * M_PI / 180) * position.x + cos(ksi * M_PI / 180) * position.y;
+            lines_1[0].position = Vector2f(X1, Y1);
+            float check = -1;
             
+            if (circles.size() != 0)
+            {
+                float min_to_cirlce = length(circles[0].getPosition().x, circles[0].getPosition().y);
+                for (int j = 0; j < circles.size(); ++j)
+                {
+                    
+                    float vect_lenght = length(X1, Y1);                             //длина вектора
+                    Vector2f norm_lenght(X1 / (vect_lenght), Y1 / (vect_lenght));  // нормирую вектор
+                    
+                    float lenght_circles = length(circles[j].getPosition().x, circles[j].getPosition().y); // c 0;0 до окр
+
+                    if (lenght_circles <= min_to_cirlce)
+                    {
+                        min_to_cirlce = lenght_circles;
+                    }
+                  
+
+                    norm_lenght.x = norm_lenght.x * lenght_circles;    //увеличиваю вектор до длины окружности
+                    norm_lenght.y = norm_lenght.y * lenght_circles;
+                    
+                    float length_in_circles = check_in_circle(norm_lenght.x, circles[j].getPosition().x, norm_lenght.y, circles[j].getPosition().y); //длина положения конца вектора до окружности
+                    if (check == 1 and lenght_circles <= min_to_cirlce)
+                    {
+                        if (length_in_circles <= circles[j].getRadius() and lenght_circles <= vect_lenght)
+                        {
+                            lines_1[0].position = Vector2f(norm_lenght.x, norm_lenght.y);
+                        }
+                    }
+                    if (length_in_circles <= circles[j].getRadius() and lenght_circles <= vect_lenght and check == -1) //проверка находится ли положения конца вектора в окружности
+                    {
+                        check = 1;
+                        lines_1[0].position = Vector2f(norm_lenght.x, norm_lenght.y);  //если да, то принимает координаты, внутри окр-ти
+                    }
+                    
+                }
+            }
+            
+            window.draw(lines_1);
         }
-        window.draw(line);
+
+        for (auto& s : circles) 
+        {
+            window.draw(s);
+
+        }
         window.display();
     }
     
