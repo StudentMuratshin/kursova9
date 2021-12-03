26c1e1bd94eb3b58a6ae2943efde8cd7d8db962b
Ильхам Муратшин <PETA4OK96@mail.ru>
2021-12-03 11:53:53 +0500

    сделал логику света для полигонов

--- kr/kr.cpp
+++ kr/kr.cpp
@@ -11,21 +11,21 @@ using namespace sf;
 using namespace std;
 
 
-class Circle: public CircleShape 
+class Circle : public CircleShape
 {
 public:
-    Circle(int R, int G, int B, float x, float y, int r) 
+    Circle(int R, int G, int B, float x, float y, int r)
     {
         R = R; G = G; B = B;
         this->setFillColor(Color(R, G, B));
-        
+
         this->setRadius(r);
-        this->setPosition(x,y);
-        this->setOrigin(r,r);
+        this->setPosition(x, y);
+        this->setOrigin(r, r);
     }
 };
 
-class polygon: public ConvexShape
+class polygon : public ConvexShape
 {
 public:
     polygon(vector <Vector2f> xy)
@@ -51,18 +51,40 @@ float length(float x1, float x2)
     return (sqrt(pow(x1, 2) + pow(x2, 2)));
 }
 
+bool intersect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float& x, float& y)
+{
+    
+    float Ua, Ub, numerator_a, numerator_b, denominator;
+
+    denominator = (y4 - y3) * (x1 - x2) - (x4 - x3) * (y1 - y2);
+    if (denominator == 0) {
+        if ((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1) == 0 && (x1 * y2 - x2 * y1) * (y4 - y3) - (x3 * y4 - x4 * y3) * (y2 - y1) == 0)
+            return true;
+        else false;
+    }
+    else {
+        numerator_a = (x4 - x2) * (y4 - y3) - (x4 - x3) * (y4 - y2);
+        numerator_b = (x1 - x2) * (y4 - y2) - (x4 - x2) * (y1 - y2);
+        Ua = numerator_a / denominator;
+        Ub = numerator_b / denominator;
+        x = Ua * x1 + (1 - Ua) * x2;
+        y = Ua * y1 + (1 - Ua) * y2;
+        return (Ua >= 0 && Ua <= 1 && Ub >= 0 && Ub <= 1);
+    }
+}
 
 int main()
 {
 
-    int phi = 30, fps = 30, type = 2;
+    int phi = 30, fps = 30, type = 2; 
+    float x, y;
     srand(time(NULL));
-   
-    
+
+
     RenderWindow window(VideoMode(), "курсовая", Style::Fullscreen);
     //window.setKeyRepeatEnabled(false);
     window.setFramerateLimit(fps);
-    
+
 
     vector <Circle> circles;    //вектор окружностей
     vector <Circle> pol;        //вектор вершин многоугольников
@@ -80,7 +102,7 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
-        
+
 
         //добавление объектов при помощи мыши
         Vector2i position = Mouse::getPosition();
@@ -89,14 +111,14 @@ int main()
         {
             type = (type + 1) % 2;
         }
-        
-        
+
+
         if (type == 1 or type == 2)                             // многульники
         {
-            static bool lock_click; 
-            if (event.type == sf::Event::MouseButtonPressed)    
+            static bool lock_click;
+            if (event.type == sf::Event::MouseButtonPressed)
             {
-                if (event.mouseButton.button == sf::Mouse::Left && lock_click != true) 
+                if (event.mouseButton.button == sf::Mouse::Left && lock_click != true)
                 {
                     point_xy.push_back(Vector2f(position.x, position.y));     //добавляет в вектор координаты
                     pol.push_back(Circle(250, 250, 0, position.x, position.y, 3));
@@ -105,19 +127,20 @@ int main()
                         polygons.push_back(polygon(point_xy));          //добавляет много-ики в вектор
                         point_xy.clear();
                     }
-                    
-                    lock_click = true; 
+
+                    lock_click = true;
                 }
             }
-            if (event.type == sf::Event::MouseButtonReleased) 
+            if (event.type == sf::Event::MouseButtonReleased)
             {
-                if (event.mouseButton.button == sf::Mouse::Left) 
+                if (event.mouseButton.button == sf::Mouse::Left)
                 {
-                    lock_click = false; 
+                    lock_click = false;
                 }
             }
         }
         
+
         if (type == 0)
         {
             static bool lock_click;
@@ -154,8 +177,8 @@ int main()
         //поворот
         float X1, Y1, X2, Y2;
         if (Keyboard::isKeyPressed(Keyboard::Up)) {
-            
-            phi = phi + 1 ;
+
+            phi = phi + 1;
         }
         if (Keyboard::isKeyPressed(Keyboard::Down)) {
 
@@ -176,15 +199,15 @@ int main()
         line[2].color = Color(255, 255, 255, 0);*/
 
         int ksi = phi;
-        
-        
+
+
 
 
         //отрисовка объектов
         window.clear(Color::Black);
 
 
-        
+
 
         //лайны
         VertexArray lines_1(LinesStrip, 2);
@@ -196,6 +219,23 @@ int main()
             Y1 = -sin(ksi * M_PI / 180) * position.x + cos(ksi * M_PI / 180) * position.y;
             lines_1[0].position = Vector2f(X1, Y1);
             float check = -1;
+
+            if (polygons.size() != 0)
+            {
+                
+                for (int i = 0; i < polygons.size(); i++)
+                {
+                    for (int j = 0; j < polygons[i].getPointCount() - 1; j++)
+                    {
+                        if (intersect(0, 0, X1, Y1, polygons[i].getPoint(j).x, polygons[i].getPoint(j).y,
+                            polygons[i].getPoint(j + 1).x, polygons[i].getPoint(j + 1).y, x, y))
+                        {
+                            lines_1[0].position = Vector2f(x, y);
+                        }
+                        
+                    }
+                }
+            }
 
             if (circles.size() != 0)
             {
@@ -234,10 +274,12 @@ int main()
                 }
             }
 
+            
+
             window.draw(lines_1);
         }
 
-        for (auto& s : circles) 
+        for (auto& s : circles)
         {
             window.draw(s);
         }
@@ -251,6 +293,6 @@ int main()
         }
         window.display();
     }
-    
+
     return 0;
 }
